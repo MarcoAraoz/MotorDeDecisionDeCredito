2ab4300242c29403dc2cdeab062ca2ad9633192e
Marco Antonio Juárez Araoz <marcojuarezaraoz@gmail.com>
2023-10-13 14:28:34 -0600

    implementing the first code

---
+++ mesesTrabajando.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")({ sigint: true });
+function fechaPrimerEmpleo(diaIngreso, mesIngreso, añoIngreso) {
+  var fechaIngreso = new Date(añoIngreso, mesIngreso - 1, diaIngreso);
+
+  var fechaActual = new Date();
+
+  var diferenciaTiempo = fechaActual - fechaIngreso;
+
+  var milisegundosPorMes = 30 * 24 * 60 * 60 * 1000;
+  var mesesTrabajando = Math.floor(diferenciaTiempo / milisegundosPorMes);
+
+  return mesesTrabajando;
+}
+// Solicitar al usuario que ingrese la fecha de ingreso
+var diaIngreso = prompt("Ingresa el día de ingreso:");
+var mesIngreso =
+  prompt("Ingresa el mes de ingreso (1 para enero, 2 para febrero, etc.):") - 1;
+var añoIngreso = prompt("Ingresa el año de ingreso:");
+
+var resultado = fechaPrimerEmpleo(diaIngreso, mesIngreso, añoIngreso);
+console.log("Meses trabajando: " + resultado);
+
+module.exports = { fechaPrimerEmpleo };
